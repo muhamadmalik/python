73dcb245625abd66b0825b238512e772d9341994
MuhamadMalik951 <muhamadmalik471@gmail.com>
2025-03-15 02:32:59 +0500

    implement twoSum function with hash map for efficient lookup and add debug print statements

--- two_sum.py
+++ two_sum.py
@@ -1,8 +1,14 @@
 def twoSum(nums, target):
-    # given an array
-    # need to find two number that equal to target
     hash_map = dict()
-    print(hash_map)
-    pass
-
+    for n in range(len(nums)):
+        difference = target - nums[n]
+        if difference in hash_map:
+           return  [ hash_map[difference], n]
+        else:
+            print(f"I'm storing {nums[n]} as key and {n} as a answer")
+            hash_map[nums[n]] = n
+    return []
 print(twoSum([2, 7, 11, 15], 9))
+
+
+
