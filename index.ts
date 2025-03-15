5df1aa990a0296e5a584c9a12ba47c2d9783ae90
MuhamadMalik951 <muhamadmalik471@gmail.com>
2025-03-15 20:25:56 +0500

    refactor isAnagram and twoSum functions for clarity; add debug print statements

--- index.ts
+++ index.ts
@@ -1,32 +1,45 @@
-function isAnagram(s:string, t: string) {
- let countS   = new Map()
- let countT = new Map()
+function isAnagram(s: string, t: string) {
+  let countS = new Map();
+  let countT = new Map();
 
- if(s.length != t.length){
-    return false
- }
-    for(const i of s){
-        countS[i] = (countS[i] || 0) + 1
-    }
-    
-    for(const i of t){
-        countT[i] = (countT[i] || 0) + 1
-    }
+  if (s.length != t.length) {
+    return false;
+  }
+  for (const i of s) {
+    countS[i] = (countS[i] || 0) + 1;
+  }
 
-    console.log(countS)
-    console.log(countT)
+  for (const i of t) {
+    countT[i] = (countT[i] || 0) + 1;
+  }
 
-    for(const i of s){
-        if(countS[i] != countT[i]){
-            return false
-        }
-        // console.log(countS[i])
-        // console.log(countT[s[i]])
-        
+  console.log(countS);
+  console.log(countT);
+
+  for (const i of s) {
+    if (countS[i] != countT[i]) {
+      return false;
     }
-    return true
+  }
+  return true;
 }
 
-const s = "rata"
-const t = "arat"
-console.log(isAnagram(s, t))
+const s = 'rata';
+const t = 'arat';
+
+function twoSum(arr, target) {
+  const storage = new Map<number, number>();
+  for (let index = 0; index < arr.length; index++) {
+    const num = arr[index];
+    const neededValue = target - num;
+    if (storage.has(neededValue)) {
+        
+      return [storage.get(neededValue), index];
+    }
+    storage.set(num, index) 
+    
+  }
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
--- two_sum.py
+++ two_sum.py
@@ -3,9 +3,8 @@ def twoSum(nums, target):
     for n in range(len(nums)):
         difference = target - nums[n]
         if difference in hash_map:
-           return  [ hash_map[difference], n]
+            return [hash_map.get(difference), n]
         else:
-            print(f"I'm storing {nums[n]} as key and {n} as a answer")
             hash_map[nums[n]] = n
     return []
 print(twoSum([2, 7, 11, 15], 9))
