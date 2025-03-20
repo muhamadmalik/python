37de3ef5e785f15f3e3d2935d6fb5a1d6438d37b
MuhamadMalik951 <muhamadmalik471@gmail.com>
2025-03-20 21:11:50 +0500

    implement merge sort algorithm in Python

---
+++ sort_an_array.py
@@ -0,0 +1,40 @@
+
+def mergeSort(arr, l, r):
+    if l == r:
+        return
+    m = (l + r) // 2
+    mergeSort(arr, l, m)
+    mergeSort(arr, m + 1, r)
+    merge(arr, l, m, r)
+    return arr
+
+
+def merge(arr, L, M, R):
+    left = arr[L:M+1]
+    right = arr[M+1:R+1]
+    
+    i = L
+    j = 0
+    k = 0
+    
+    while j < len(left) and k < len(right):
+        if left[j] <= right[k]:
+            arr[i] = left[j]
+            j = j + 1
+        else:
+            arr[i] = right[k]
+            k = k + 1
+        i = i + 1
+    while j < len(left):
+        arr[i] = left[j]
+        j += 1
+        i += 1
+    while k < len(right):
+        arr[i] = right[k]
+        i = i + 1
+        k = k + 1
+
+
+
+nums = [2, 1, 3, 23, 434, 23909, 232]
+print(mergeSort(nums, 0, len(nums)))
