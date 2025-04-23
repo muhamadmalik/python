c86c10ccb232d7bd9e5183681bf519ff7aeaae4c
MuhamadMalik951 <muhamadmalik471@gmail.com>
2025-04-23 14:12:49 +0500

    refactor reverseString function to use recursion for improved readability

--- reverse_string.py
+++ reverse_string.py
@@ -1,14 +1,23 @@
-def reverseString(s):
-    stack = []
-    i = 0
-    for c in s:
-        stack.append(c) 
-    while stack:
-        s[i] = stack.pop()
-        i = i + 1
-    return s
-    
-    
+# def reverseString(s):
+#     stack = []
+#     i = 0
+#     for c in s:
+#         stack.append(c) 
+#     while stack:
+#         s[i] = stack.pop()
+#         i = i + 1
+#     return s
+        
+# def reverseString(s):
+#     def reverse(l, r):
+#         if l < r:
+#             reverse(l + 1, r - 1)
+#             temp = s[l]
+#             s[l] = s[r]
+#             s[r] = temp
+#     reverse(0, len(s) - 1)
+#     return s
+
 # def reverseString(s):
 #     l = 0
 #     r = len(s) - 1
