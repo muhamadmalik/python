f7f6ab75688f323f75d4b530ae811305f510b850
MuhamadMalik951 <muhamadmalik471@gmail.com>
2025-04-25 02:18:11 +0500

    implement function to check if a string is a palindrome, ignoring non-alphanumeric characters

---
+++ is_palindrome.py
@@ -0,0 +1,20 @@
+def isPalindrome(s ) :
+    l = 0
+    r = len(s) - 1
+    def isAlphaNum(c):
+        return ord('A') <=  ord(c) <= ord('Z') or ord('a') <= ord(c) <= ord('z') or ord('0') <= ord(c) <= ord('9')    
+    while l < r:
+        while l < r and  not isAlphaNum(s[l]):
+            l += 1
+        while l < r and not isAlphaNum(s[r]):
+            r -= 1
+            
+        if s[l].lower() != s[r].lower():
+            return False
+        
+        l += 1
+        r -= 1
+    return True    
+
+
+print(isPalindrome('A man, a plan, a canal: Panamad'))
