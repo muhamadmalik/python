bef8c4e5ef240766aca32d1f5229a33aed745465
MuhamadMalik951 <muhamadmalik471@gmail.com>
2025-06-30 10:39:17 +0500

    implement function to return the largest number less than the given input; add example usage

---
+++ DSA/one_less_than_given_number.py
@@ -0,0 +1,12 @@
+def one_less_than_given(number):
+    num = 0
+    
+    for n in range(number):
+        num = n
+    return num
+    
+    pass
+
+
+result = one_less_than_given(22)
+print(result)
